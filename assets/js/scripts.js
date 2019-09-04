34f53e9f86442c8b46806d938b99629fab6aa0f5
tammy <tammy.jacobus@creativespark.co.za>
2019-09-04 17:49:42 +0200

    CTICC Repo - FE

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$('li.men').hover(
+    function () { $('.main-menu-inner').addClass('show-menu-inner') },
+    function () { $('.main-menu-inner').removeClass('show-menu-inner') }
+)
+
+$('.main-menu-inner').hover(
+    function () { $('.main-menu-inner').addClass('show-menu-inner') },
+    function () { $('.main-menu-inner').removeClass('show-menu-inner') }
+)
